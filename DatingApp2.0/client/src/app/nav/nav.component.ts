cef1cb82fa150d7a43f7f69d08d93a4df1ba303b
Tony Qiao <tony419c@gmail.com>
2023-08-28 18:46:41 +1000

    1. Added a currentUserSource as BehaviorSubject in account.service to be used for an in-memory queue/indicator of whether user has logged in; 2. Also added a currentUser$ as an Observable of currentUserSource ; 3. Changed the login() and logout() methods in account.service to store/remove logged-in user both in the browser's persistent localStorage and in-memory currentUserSource; 4. Changed app.component's ngOnInit() to read the logged-in user from browser's persistent localStorage and store it in currentUserSource; 5. Changed nav.component to use (currentUser$ | async) instead of the previous boolean variable LoggedIn.

--- DatingApp2.0/client/src/app/nav/nav.component.ts
+++ DatingApp2.0/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,26 +10,25 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit{
   model: any = {};
-  loggedIn = false;
+  currentUser$: Observable<User | null> = of(null);
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
-
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error1 => console.log(error1)
     });
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
