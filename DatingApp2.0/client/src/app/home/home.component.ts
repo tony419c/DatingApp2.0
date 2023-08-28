cbffd575feaad44fbd0076a7382b1f2de69b98d8
Tony Qiao <tony419c@gmail.com>
2023-08-28 19:10:30 +1000

    Added a home.component and put "Register" and "Learn more" buttons on the page. Also reserved the div for register form.

---
+++ DatingApp2.0/client/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode = false;
+
+  constructor() { }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+}
