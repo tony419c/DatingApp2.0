e0c0d16dd5e31ce1f0b4a81114ebd1e53d9d759b
Tony Qiao <tony419c@gmail.com>
2023-08-30 00:39:34 +1000

    1. Added a register.component (serving the register form) which will be a child component of the home.component; 2. Moved the getUsers() http.get logic etc. from app.component to home.component; 3. Made it pass users that home.component gets from the backend to register.component by using Angular parent to child communication mechanism.

--- DatingApp2.0/client/src/app/home/home.component.ts
+++ DatingApp2.0/client/src/app/home/home.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+    ngOnInit(): void {
+      this.getUsers();
+    }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error1 => console.log(error1),
+      complete: () => console.log('Request has completed')
+    })
   }
 }
---
+++ DatingApp2.0/client/src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  @Input() usersFromHomeComponent: any;
+
+  model: any = {}
+
+  constructor() { }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+
+}
