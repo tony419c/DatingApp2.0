0c08383a899e29ce0ba8cdb092c4591362414d3a
Tony Qiao <tony419c@gmail.com>
2023-09-25 14:50:01 +1000

    1. Extended the model class of AppUser.cs to support more user information related to Dating App; 2. Added a model class of Photo.cs to store basic information of each user profile photo.

--- DatingApp2.0/Entities/AppUser.cs
+++ DatingApp2.0/Entities/AppUser.cs
@@ -9,5 +9,28 @@
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        //public List<Photo> Photos { get; set; } = new List<Photo>();
+        public List<Photo> Photos { get; set; } = new();
     }
 }
---
+++ DatingApp2.0/Entities/Photo.cs
@@ -0,0 +1,13 @@
+﻿namespace DatingApp2._0.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsMain { get; set; }
+
+        public string PublicId { get; set; }
+    }
+}
