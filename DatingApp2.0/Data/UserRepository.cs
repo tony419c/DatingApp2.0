f920a6a9280b079ab624054148d1204d88e5112d
Tony Qiao <tony419c@gmail.com>
2023-09-27 22:51:37 +1000

    Added an interface of IUserRepository and its implementation class of UserRepository.

---
+++ DatingApp2.0/Data/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using DatingApp2._0.Entities;
+using DatingApp2._0.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp2._0.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
---
+++ DatingApp2.0/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp2._0.Entities;
+
+namespace DatingApp2._0.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+
+        Task<bool> SaveAllAsync();
+
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+
+        Task<AppUser> GetUserByIdAsync(int id);
+
+        Task<AppUser> GetUserByUsernameAsync(string username);
+    }
+}
