43987476c2b5c08d7901c7ca0c2624038f7dd9c2
Tony Qiao <tony419c@gmail.com>
2023-10-01 17:41:26 +1100

    1. Added reference and logic to use ng-gallery in the member-detail.component to show user photos; 2. Included a default user profile photo to the Angular assets folder; 3. Made sure that both member-detail.component and member-card.component fall back to the default user.png if user doesn't have a main photo.

--- DatingApp2.0/client/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp2.0/client/src/app/members/member-detail/member-detail.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Member } from '../../_models/member';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit{
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
@@ -21,8 +27,18 @@ export class MemberDetailComponent implements OnInit{
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member;
+        this.getImages();
+      }
     })
+  }
+
+  getImages() {
+    if (!this.member) return;
+    for (const photo of this.member?.photos) {
+      this.images.push(new ImageItem({src: photo.url, thumb:photo.url}))
+    }
   }
 
 }
