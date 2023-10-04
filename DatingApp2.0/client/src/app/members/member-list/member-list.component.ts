0c65dda04e22faf13be8c9ea3066a828b8fbe702
Tony Qiao <tony419c@gmail.com>
2023-10-04 14:42:16 +1100

    In order to keep the members list that Angular gets from the backend API, we now choose to store the members list in member.service (because Angular service has a lifetime throughtout the whole Angular client application), instead of keeping them in the member-list.component (anything in an Angular component only has a lifetime from the creation to the destruction of that component.
    1. Changed member.service logic to persistently keep the members list in that service;
    
    2. member-list.component will no longer explicitly subscribe to memberService.getMembers() and store what it gets to the members list that is local to member-list.component. Insteand, it will only get memberService.getMembers() in an unsubscribed way and keep the Observable in the component. Now it's up to member-list.component.html to use the async pipe to subscribe to it.

--- DatingApp2.0/client/src/app/members/member-list/member-list.component.ts
+++ DatingApp2.0/client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../../_models/member';
 import { MembersService } from '../../_services/members.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-member-list',
@@ -8,18 +9,12 @@ import { MembersService } from '../../_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-
-  loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.members$ = this.memberService.getMembers();
   }
 
 }
