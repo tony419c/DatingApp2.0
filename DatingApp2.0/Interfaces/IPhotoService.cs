deb0d164beacf8b0f3ba11f2d11abf1c6f2afabc
Tony Qiao <tony419c@gmail.com>
2024-01-31 10:32:02 +1100

    A test commit after re-installing VS2022

--- DatingApp2.0/Interfaces/IPhotoService.cs
+++ DatingApp2.0/Interfaces/IPhotoService.cs
@@ -5,7 +5,6 @@ namespace DatingApp2._0.Interfaces
     public interface IPhotoService
     {
         Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
-
         Task<DeletionResult> DeletePhotoAsync(string publicId);
     }
 }
